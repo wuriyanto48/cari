a8c6fbc2e7c927fdf2acd42e2329fdb641d1bab3
wuriyanto <wuriyanto48@yahoo.co.id>
2019-02-14 14:35:29 +0700

    add argument struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+
+const VERSION: &'static str = "0.0.0";
+
+#[derive(Debug)]
+pub struct Arguments {
+    pub flag: String,
+    pub keyword: String,
+    pub file_name: String,
+    pub threads: u32,
+}
+
+pub struct Output (String, u64);
+
 #[cfg(test)]
 mod tests {
     #[test]
